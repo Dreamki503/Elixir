f538eac59f584da42fba81cac3c42b409de75689
Aryan Leon Jude Mechery <aryanleon503@gmail.com>
2024-08-19 09:17:23 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,6 +3,9 @@ import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import speech_recognition as sr
 from textblob import TextBlob
+import nltk
+
+nltk.download("wordnet")
 
 # Initialize floating elements if needed
 from streamlit_float import *
